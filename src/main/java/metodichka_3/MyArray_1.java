486639e1618c893f18d91250ae3b5c826974821c
RmnITLL <inetmilo@rambler.ru>
2022-03-24 11:01:47 +0300

    first commit

---
+++ src/main/java/metodichka_3/MyArray_1.java
@@ -0,0 +1,14 @@
+package metodichka_3;
+
+public class MyArray_1 {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = i;
+            System.out.println("arr[" + i + "] = " + arr[i]);
+        }
+    }
+}
